74d94b8a98c70a2ab4d0b8ced9dc1350d95d36d9
Gabriel de Perthuis <g2p.code@gmail.com>
2018-09-26 22:57:17 +0200

    Start refactoring futures

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,9 @@ extern crate toml;
 extern crate webpki_roots;
 
 use directories::ProjectDirs;
+use futures::prelude::*;
 use futures::future::{err, ok, Either};
 use hyper::client::HttpConnector;
-use hyper::rt::Future;
-use hyper::rt::Stream;
 use hyper::service::service_fn;
 use hyper::{Client, Server};
 use hyper_rustls::HttpsConnector;
@@ -67,6 +66,30 @@ fn write_token(token_path: &PathBuf, token: &str) {
     std::fs::rename(&new_path, token_path).unwrap();
 }
 
+fn load_token_async(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    conf: &toml::Value,
+) -> impl Future<Item = Option<String>, Error = ()> {
+    let username = conf["username"].as_str().unwrap();
+    let password = conf["password"].as_str().unwrap();
+    let device_id = conf["device-id"].as_str().unwrap();
+    client
+        .request(gpsoauth::master_login_request(
+            username, password, device_id,
+        )).and_then(move |res| {
+            res.into_body().fold(None, |acc, chunk| {
+                ok::<_, hyper::Error>(chunk.lines().fold(acc, |acc, line| {
+                    let line = line.unwrap();
+                    if line.starts_with("Token=") {
+                        Some(line[6..].to_owned())
+                    } else {
+                        acc
+                    }
+                }))
+            })
+        }).map_err(|e| eprintln!("Login error {}", e))
+}
+
 fn main() {
     env_logger::init();
     let dirs = ProjectDirs::from("com.github", "G2P", "Music Server").unwrap();
@@ -77,12 +100,6 @@ fn main() {
         conf_file.read_to_string(&mut conf_buf).unwrap();
         conf0 = conf_buf.parse::<toml::Value>().unwrap();
         conf = &conf0[CONF_ROOT];
-    }
-    let (username, password, device_id);
-    {
-        username = conf["username"].as_str().unwrap();
-        password = conf["password"].as_str().unwrap();
-        device_id = conf["device-id"].as_str().unwrap();
     }
 
     let mut token_path = std::path::PathBuf::from(dirs.data_dir());
@@ -102,46 +119,28 @@ fn main() {
     let https = HttpsConnector::from((http, tls_config));
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    let login_req = client.request(gpsoauth::master_login_request(
-        username, password, device_id,
-    ));
+    let main_future = load_token_async(&client, &conf).and_then(move |token_opt| {
+        if let Some(token) = token_opt {
+            println!("Token is {}", token);
+            write_token(&token_path, &token);
+            println!("Listening on {}", addr);
+            let proxy_svc = move || {
+                let client = client.clone();
+                service_fn(move |req| {
+                    println!("Proxying {}", req.uri().path());
+                    client.get("http://google.fr/".parse().unwrap())
+                })
+            };
 
-    let main_future = login_req
-        .map_err(|e| eprintln!("Login error: {}", e))
-        .and_then(move |res| {
-            res.into_body()
-                .fold(None, |acc, chunk| {
-                    ok::<_, hyper::Error>(chunk.lines().fold(acc, |acc, line| {
-                        let line = line.unwrap();
-                        if line.starts_with("Token=") {
-                            Some(line[6..].to_owned())
-                        } else {
-                            acc
-                        }
-                    }))
-                }).map_err(|e| panic!("Error: {:?}", e))
-        }).and_then(move |token_opt| {
-            if let Some(token) = token_opt {
-                println!("Token is {}", token);
-                write_token(&token_path, &token);
-                println!("Listening on {}", addr);
-                let proxy_svc = move || {
-                    let client = client.clone();
-                    service_fn(move |req| {
-                        println!("Proxying {}", req.uri().path());
-                        client.get("http://google.fr/".parse().unwrap())
-                    })
-                };
+            let server = Server::bind(&addr)
+                .serve(proxy_svc)
+                .map_err(|e| eprintln!("Server error: {}", e));
 
-                let server = Server::bind(&addr)
-                    .serve(proxy_svc)
-                    .map_err(|e| eprintln!("Server error: {}", e));
-
-                Either::A(server)
-            } else {
-                Either::B(err(eprintln!("Failed to log in")))
-            }
-        });
+            Either::A(server)
+        } else {
+            Either::B(err(eprintln!("Failed to log in")))
+        }
+    });
 
     println!("Starting up");
     hyper::rt::run(main_future);
