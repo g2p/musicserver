9a4bccec8857345cde63cb50e5d864ef0d23ad7c
Gabriel de Perthuis <g2p.code@gmail.com>
2018-09-28 21:47:55 +0200

    Reuse existing tokens whenever possible

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,11 @@ extern crate serde_json;
 extern crate toml;
 extern crate webpki_roots;
 
+use gpsoauth::AuthContext;
+use std::path::Path;
 use directories::ProjectDirs;
 use futures::prelude::*;
+use futures::future;
 use gpsoauth::AuthorizedRequestBuilder;
 use hyper::client::HttpConnector;
 use hyper::service::service_fn;
@@ -69,6 +72,15 @@ fn write_token(token_path: &PathBuf, token: &str) {
     std::fs::rename(&new_path, token_path).unwrap();
 }
 
+fn read_token(token_path: &Path) -> std::io::Result<String> {
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .open(token_path)?;
+    let mut r = String::new();
+    file.read_to_string(&mut r)?;
+    Ok(r)
+}
+
 fn main() {
     env_logger::init();
     let dirs = ProjectDirs::from("com.github", "G2P", "Music Server").unwrap();
@@ -104,7 +116,7 @@ fn main() {
     let password = conf["password"].as_str().unwrap().to_owned();
     let device_id = conf["device-id"].as_str().unwrap().to_owned();
 
-    let ctx = gpsoauth::AuthContext {
+    let mut ctx = AuthContext {
         client: Box::new(client),
         username,
         password,
@@ -118,40 +130,61 @@ fn main() {
         oauth_token: None,
     };
 
-    let main_future = gpsoauth::master_auth_async(ctx).and_then(move |ctx| {
-        let master_token = ctx.master_token.clone().unwrap();
-        println!("Master token is {}", master_token);
-        write_token(&master_token_path, &master_token);
-        gpsoauth::oauth_async(ctx)
-    }).and_then(move |ctx| {
-        let oauth_token = ctx.oauth_token.clone().unwrap();
-        println!("OAuth token is {}", oauth_token);
-        write_token(&oauth_token_path, &oauth_token);
-        ctx.client.request(hyper::Request::get("https://mclients.googleapis.com/sj/v2.5/devicemanagementinfo?alt=json&hl=en_US&dv=0&tier=fr").authorization_header(&oauth_token).body(hyper::Body::from("")).unwrap())
-            .map_err(|e| eprintln!("API error: {}", e))
-            .map(|resp| (resp, ctx))
-    }).and_then(|(resp, ctx)| {
-        resp.into_body().concat2()
-            .map_err(|e| eprintln!("API error (body chunks): {}", e))
-            .map(|body| (body, ctx))
-    }).and_then(move |(body, ctx)| {
-        //println!("Devices: {:#?}", serde_json::from_slice::<serde_json::Value>(&body));
-        println!("{}", "Devices: ".to_owned() + std::str::from_utf8(&body).unwrap());
-        println!("Listening on {}", addr);
-        let proxy_svc = move || {
-            let client = objekt::clone_box(&*ctx.client);
-            service_fn(move |req| {
-                println!("Proxying {}", req.uri().path());
-                client.request(Request::get("http://google.fr/").body(hyper::Body::from("")).unwrap())
-            })
-        };
+    let api_req_future = move |ctx: AuthContext| ctx.client.request(
+        hyper::Request::get(
+                "https://mclients.googleapis.com/sj/v2.5/devicemanagementinfo?alt=json&hl=en_US&dv=0&tier=fr"
+            ).authorization_header(&ctx.oauth_token.clone().unwrap())
+            .body(hyper::Body::from(""))
+            .unwrap())
+        .map_err(|e| eprintln!("API error: {}", e))
+        .map(|resp| (resp, ctx))
+        .and_then(|(resp, ctx)| {
+            resp.into_body().concat2()
+                .map_err(|e| eprintln!("API error (body chunks): {}", e))
+                .map(|body| (body, ctx))
+        }).and_then(move |(body, ctx)| {
+            //println!("Devices: {:#?}", serde_json::from_slice::<serde_json::Value>(&body));
+            println!("{}", "Devices: ".to_owned() + std::str::from_utf8(&body).unwrap());
+            println!("Listening on {}", addr);
+            let proxy_svc = move || {
+                let client = objekt::clone_box(&*ctx.client);
+                service_fn(move |req| {
+                    println!("Proxying {}", req.uri().path());
+                    client.request(Request::get("http://google.fr/").body(hyper::Body::from("")).unwrap())
+                })
+            };
 
-        let server = Server::bind(&addr)
-            .serve(proxy_svc)
-            .map_err(|e| eprintln!("Server error: {}", e));
+            let server = Server::bind(&addr)
+                .serve(proxy_svc)
+                .map_err(|e| eprintln!("Server error: {}", e));
 
-        server
-    });
+            server
+        });
+
+    let main_future;
+    if let Ok(oauth_token) = read_token(&oauth_token_path) {
+        ctx.oauth_token = Some(oauth_token);
+        main_future = future::Either::A(api_req_future(ctx));
+    } else {
+        let get_oauth_future;
+        if let Ok(master_token) = read_token(&master_token_path) {
+            ctx.master_token = Some(master_token);
+            get_oauth_future = future::Either::A(gpsoauth::oauth_async(ctx));
+        } else {
+            get_oauth_future = future::Either::B(gpsoauth::master_auth_async(ctx).and_then(move |ctx| {
+                let master_token = ctx.master_token.clone().unwrap();
+                println!("Master token is {}", master_token);
+                write_token(&master_token_path, &master_token);
+                gpsoauth::oauth_async(ctx)
+            }))
+        }
+        main_future = future::Either::B(get_oauth_future.and_then(move |ctx| {
+            let oauth_token = ctx.oauth_token.clone().unwrap();
+            println!("OAuth token is {}", oauth_token);
+            write_token(&oauth_token_path, &oauth_token);
+            api_req_future(ctx)
+        }));
+    }
 
     println!("Starting up");
     hyper::rt::run(main_future);
